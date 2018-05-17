40d92c94a7538f3cb77b2e495ba17e7ecb2eccff
Jonaljung1991 <32454645+Jonaljung1991@users.noreply.github.com>
2018-05-17 14:26:35 +0200

    Added static version of showproducts

---
+++ src/components/showproducts/showproducts.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import "./showproducts.css";
+const showProducts = () => {
+    return (<div class="component showproducts-container">
+        <div><h1>Product-List-Here</h1></div>
+        <div><ul>
+            <li>Item1</li>
+            <li>Item2</li>
+            <li>Item3</li>
+        </ul>
+        </div>
+    </div>)
+}
+export default showProducts;
