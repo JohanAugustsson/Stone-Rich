2a00d2c66835c76fae41eb771282f8a0a51a3897
Jonaljung1991 <32454645+Jonaljung1991@users.noreply.github.com>
2018-05-17 13:34:02 +0200

    Added static version of customer-container

---
+++ src/containers/customer-page/customer.js
@@ -0,0 +1,8 @@
+import React , {Component} from "react";
+// import productlist from "../mocks/products.json";
+import style from "./customer.css";
+
+const customer = () =>{
+  return(<div className="component customer-container"><h1>Customer-container</h1></div>);
+}
+export default customer;
