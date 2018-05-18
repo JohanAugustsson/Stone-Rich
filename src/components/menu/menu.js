892476485032811c33044f94f47e2284c434634c
Elin Jakobsson <elin-jakobsson@hotmail.com>
2018-05-18 16:08:27 +0200

    menu css done but not resonsive

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,17 +1,16 @@
-import React, {Component} from "react";
+import React from "react";
 import "./menu.css";
 import MenuBasket from "../menubasket/menubasket";
-const menu = () => {
-    return (<div className="component menu-container">
-        <div>
-            <h1>Menu-Container</h1>
 
-        </div>
-        <div>
-            <div><img alt="StoneRich-Img"/></div>
-            <div><MenuBasket/></div>
-            <div><p>Logga in</p></div>
-        </div>
-    </div>)
+const menu = () => {
+    return (<div className="menu-container">
+
+                <img src='img/stone-rich-logo.png' alt="StoneRich-Img"/>
+                <div className='menubasket-adminlogin-holder'>
+                  <MenuBasket onClick={()=>console.log('menubasket')}/>
+                  <p>|</p>
+                  <p className='adminlogin' onClick={()=>console.log('login admin')}>Logga in</p>
+                </div>
+            </div>)
 }
 export default menu;
