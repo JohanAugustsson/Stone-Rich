cf4737f0dcdb859fec3916ee4f7f6c29ff44add6
Johan Augustsson <j.augustsson80@gmail.com>
2018-05-20 11:08:05 +0200

    products boxes

--- src/components/showproducts/showproducts.js
+++ src/components/showproducts/showproducts.js
@@ -1,25 +1,38 @@
 import React, {Component} from "react";
 import propTypes from "prop-types";
 import "./showproducts.css";
+
+
 class showProducts extends Component {
-    componentDidMount() {}
+    componentDidMount() {
+
+    }
+
+    makeProductUl = (products) => {
+      const newProductsList =  products.map( product => {
+        return <li key={product.id}>{product.name}</li>
+      })
+
+      return (<ul> { newProductsList } </ul>)
+    }
 
     render() {
-        const products = [...this.props.productList];
-        const productList = products.map(product => <li key={product.id}>{product.name}</li>)
+        const products = this.props.productList;
+        let productList = this.makeProductUl(products);
 
-        return (<div className="component showproducts-container">
+
+        return (
+          <div className="component showproducts-container">
             <div>
-                <h1>Product-List-Component</h1>
+              <h1>Product-List-Component</h1>
             </div>
-            <div>
-                <ul>
-                    {productList}
-                </ul>
+            <div className="wrapper-products">
+              {productList}
             </div>
-        </div>)
-    }
-}
+          </div>
+        );
+    };
+};
 
 // Defining proptypes for this component
 showProducts.propTypes = {
