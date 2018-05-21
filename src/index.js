dcb4c62b5d5b5e71911741537db454c47ac258be
johan augustsson <j.augustsson80@gmail.com>
2018-05-21 21:10:57 +0200

    redux init with first reducer and action for add to basket

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import productList from "./mocks/products.json";
+import rootReducer from "./reducers/reducers";
+const initialState = {
+  basket: [],
+  isAdmin : {
+    password:"ADMIN",
+    locked : true
+  },
+  products:[...productList],
+  history:{
+    past:[],
+    present:[],
+    future:[]
+  }
+}
+const store =  createStore(rootReducer,initialState,
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
