a64124ce93cc8e7e19c847529fb1bfde7dbf4d1b
Jonaljung1991 <32454645+Jonaljung1991@users.noreply.github.com>
2018-05-22 14:01:00 +0200

    Made sure that user can not add more than existing prdoucts

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,30 +1,42 @@
-import {ADD_TO_BASKET, REMOVE_FROM_BASKET , TOGGLE_LOGIN_MENU , IS_ADMIN} from './constants.js'
+import {
+  ADD_TO_BASKET,
+  REMOVE_FROM_BASKET,
+  TOGGLE_LOGIN_MENU,
+  IS_ADMIN,
+  REMOVE_FROM_NUMBERINSTORE
+} from './constants.js'
 
-let addToBasket = ( nb, id ) => {
-  return (
-    {
-      type: ADD_TO_BASKET,
-      nb,
-      id
-    }
-  )
+let addToBasket = (nb, id) => {
+  return ({
+    type: ADD_TO_BASKET,
+    nb,
+    id
+  })
+}
+let removeFromNumberInstore = (nb, id) => {
+  return ({
+    type: REMOVE_FROM_NUMBERINSTORE,
+    nb,
+    id
+  })
 }
 
 let toggleLoginMenu = () => {
-  return (
-    {
-      type: TOGGLE_LOGIN_MENU
-    }
-  )
+  return ({
+    type: TOGGLE_LOGIN_MENU
+  })
 }
 
 let isAdmin = (isAdmin) => {
-  return (
-    {
-      type: IS_ADMIN,
-      isAdmin
-    }
-  )
+  return ({
+    type: IS_ADMIN,
+    isAdmin
+  })
 }
 
-export { addToBasket, toggleLoginMenu, isAdmin };
+export {
+  addToBasket,
+  toggleLoginMenu,
+  isAdmin,
+  removeFromNumberInstore
+};
